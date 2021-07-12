935a53d5e5dbba6437ce378d14d44e761923e097
amcelroy50744 <85271297+amcelroy50744@users.noreply.github.com>
2021-07-12 15:58:29 -0400

    Add files via upload

---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount();
+            Console.WriteLine("How much would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"{account.GetBalance()}");
+        }
+    }
+}
---
+++ BankEncapsulation/README.md
@@ -0,0 +1,1 @@
+"# EncapsulationEx" 
