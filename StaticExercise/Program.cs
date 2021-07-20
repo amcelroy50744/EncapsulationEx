25ff27e7fa4f07f9ce02184db73c4e45c4e1c063
amcelroy50744 <85271297+amcelroy50744@users.noreply.github.com>
2021-07-20 13:39:04 -0400

    Add files via upload

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert degrees in Fahrenheit ");
+            var celcius = TempCoverter.FahrenheitTOCelsius(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine($"celcius {celcius}");
+            Console.WriteLine("Please insert degrees in Celcius");
+            var fahrenheit = TempCoverter.CelsiusToFahrenheit(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine($"fahrenheit {fahrenheit}");
+        }
+    }
+}
